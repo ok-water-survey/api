dd71b6c4e13b19f079af629c8db8e89d5832ad1d
Mark Stacy <mbstacy@gmail.com>
2015-01-14 10:49:40 -0600

    Fabric file for deployment

---
+++ fabfile.py
@@ -0,0 +1,147 @@
+from fabric.api import *
+from fabric.contrib.files import exists, append, comment
+from fabric.colors import red
+import os 
+
+env.sitename = os.path.basename(os.getcwd())
+env.mongo_host = 'test.oklahomawatersurvey.org'
+env.psql_host = 'test.oklahomawatersurvey.org'
+env.apache_config = '/etc/httpd/conf.d/%(sitename)s.conf' % env
+env.python = '/usr/bin/python2.7'
+
+ 
+def ows_test():
+    """
+    Work on staging environment
+    """
+    env.settings = 'ows_test'
+    env.static_dirs = '/var/www/html/static/%(sitename)s' % env
+    env.db_name = 'db.sqlite3'
+    env.path = '/var/www/apps/%(sitename)s' % env
+    env.virtpy = '%(path)s/virtpy' % env
+    env.log_path = '%(path)s/log' % env
+    env.host = 'test.oklahomawatersurvey.org'
+    env.hosts = ['test.oklahomawatersurvey.org']
+
+def ows_data():
+    """
+    Work on production environment
+    """
+    env.settings = 'ows_data'
+    env.path = '/var/www/apps/%(sitename)s' % env
+    env.virtpy = '%(path)s/virtpy' % env
+    env.log_path = '%(path)s/log' % env
+    env.hosts = ['data.oklahomawatersurvey.org']
+    
+def setup():
+    """ 
+    Setup directories and copy everything but virtual environment to server,
+        then install virtual environment based on requirements.txt
+    """
+    setup_directories()
+    copy_working_dir()
+    setup_virtualenv()
+    install_requirements()
+    apache_config()
+    bounce_apache()
+
+def deploy():
+    """
+    Deploy changes which don't impact virtual environment
+    """ 
+    init_deploy_procedure()
+    copy_working_dir()
+    custom_procedure()
+    bounce_apache()
+
+def init_deploy_procedure():
+    local('scp %(host)s:%(path)s/%(db_name)s .' % env)
+
+def setup_directories():
+    """ 
+    Setup directories on the remote system
+    """
+    if not exists('%(path)s' % env):
+        run('mkdir -p %(path)s' % env)
+        run('mkdir -p %(log_path)s' % env)
+        run('mkdir -p %(virtpy)s' % env)
+
+
+def virtualenv(command):
+    """ 
+    Wrapper to activate and run virtual environment
+    """
+    with cd(env.virtpy):
+        run('source %(virtpy)s/bin/activate' % env + '&&' + command)
+
+def setup_virtualenv():
+    """
+    Install the virtual environment
+    """
+    run('virtualenv -p %(python)s --no-site-packages %(virtpy)s' % env)
+
+def bounce_apache():
+    """ Restart the apache web server """
+    sudo('/etc/init.d/httpd restart')
+
+def apache_config(secure=False):
+    """
+    Set the apache config file to point to wsgi.  Assumes app will be accessible at /sitename/ and 
+      .wsgi named sitename.wsgi
+    """
+    # check if apache config lines exist in old wsgi_sites.conf and comment if found
+    comment('/etc/httpd/conf.d/wsgi_sites.conf', r'^WSGIScriptAlias /%(sitename)s .*$' % env, use_sudo=True)
+    confline = 'WSGIScriptAlias /%(sitename)s %(path)s/%(sitename)s.wsgi' %env
+    append('%(apache_config)s' % env, confline, use_sudo=True)
+    if secure:
+        secure_app = """
+<Location /%(sitename)s>
+  AuthType Basic 
+  require valid-user
+  TKTAuthLoginURL http://test.cybercommons.org/accounts/login/
+  TKTAuthTimeoutURL http://test.cybercommons.org/accounts/login/?timeout=1 
+  TKTAuthPostTimeoutURL http://test.cybercommons.org/accounts/login/?posttimeout=1 
+  TKTAuthUnauthURL http://test.cybercommons.org/accounts/login/?unauth=1 
+  TKTAuthIgnoreIP on
+  TKTAuthBackArgName next
+</Location>
+""" % (env)
+        append('%(apache_config)s' % env, secure_app, use_sudo=True)
+
+def copy_working_dir():
+    """
+    Shuttle application code from local to remote
+    """
+    local('tar --exclude .git --exclude virtpy -czf /tmp/deploy_%(sitename)s.tgz .' % env)
+    put('/tmp/deploy_%(sitename)s.tgz' % env, '%(path)s/deploy_%(sitename)s.tgz' % env)
+    run('cd %(path)s; tar -xf deploy_%(sitename)s.tgz; rm deploy_%(sitename)s.tgz' % env)
+    local('rm /tmp/deploy_%(sitename)s.tgz' % env)
+
+def custom_procedure():
+    #Static Files
+    sudo('mkdir -p %(static_dirs)s' % env)
+    sudo('cd %(path)s;cp -r static/%(sitename)s/* %(static_dirs)s' % env)
+    #Sqlite database permission
+    #print 'chown -R mstacy:ows %(path)s; chmod -r 775 %(path)s;' % env
+    sudo('chown -R mstacy:ows %(path)s; chmod -R 775 %(path)s;' % env)
+
+def install_requirements():
+    """ 
+    Install the contents of requirements.txt to the virtual environment 
+    """
+    check = exists('%(path)s/requirements.txt' % env)
+    if check:
+        virtualenv('pip install -E %(virtpy)s -r %(path)s/requirements.txt' % env)
+    else:
+        print red("Can't find requirements.txt!")
+
+def upgrade_requirements():
+    """ 
+    Install the contents of requirements.txt to the virtual environment 
+    """
+    put('requirements.txt', '%(path)s/requirements.txt' % env)
+    check = exists('%(path)s/requirements.txt' % env)
+    if check:
+        virtualenv('pip install --upgrade -E %(virtpy)s -r %(path)s/requirements.txt' % env)
+    else:
+        print red("Can't find requirements.txt!")
